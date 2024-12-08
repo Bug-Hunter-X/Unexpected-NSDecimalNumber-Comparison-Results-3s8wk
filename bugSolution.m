33ad06e7ce75d79d37f92b5418b4c6364dd5e7e1
BugHunter-X <askzenai@gmail.com>
2024-12-08 15:20:06 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,4 @@
+The correct way to compare `NSDecimalNumber` objects is to use the `NSComparisonResult` enum values directly:
+
+```objectivec
+NSDecimalNumber *decimal1 = [NSDecimalNumber decimalNumberWithString:@
